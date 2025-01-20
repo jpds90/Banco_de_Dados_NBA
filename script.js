86cad40099a3de08c7a45d01712333ac4aa81c4d
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-01-20 13:06:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ const { Pool } = require('pg');
 const sleep = require('sleep-promise');
 
 // Configuração do pool de conexão ao banco de dados
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, // Usando a URL completa
+  ssl: { rejectUnauthorized: false },
 });
 
 // Função para buscar links da tabela 'links'
