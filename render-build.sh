c2906f6ba10de822a865dae6b4fc7052f742d287
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-01-21 12:54:58 +0000

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -6,6 +6,7 @@ apt-get install -y wget gnupg libxshmfence1
 
 # Instalar pacotes adicionais necessários para rodar o Chromium em ambiente headless
 apt-get install -y --no-install-recommends \
+    ca-certificates \
     fonts-liberation \
     libappindicator3-1 \
     libatk-bridge2.0-0 \
@@ -14,7 +15,11 @@ apt-get install -y --no-install-recommends \
     libdbus-glib-1-2 \
     libgbm-dev \
     libnspr4 \
+    libdbus-1-3 \
     libnss3 \
+    libdrm2 \
+    libxdamage1 \
+    libx11-xcb1 \
     libxcomposite1 \
     libxrandr2 \
     xdg-utils \
