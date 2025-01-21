daae4b490cd6deff4c633267bd9fd7b5f3ff35a6
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-01-21 22:55:05 +0000

    Update alter_tables_auto_increment.sql

--- alter_tables_auto_increment.sql
+++ alter_tables_auto_increment.sql
@@ -8,9 +8,26 @@ BEGIN
         WHERE column_name = 'id'
           AND table_schema = 'public'
     LOOP
-        EXECUTE format(
-            'ALTER TABLE %I ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1);',
-            tbl.table_name
-        );
+        BEGIN
+            -- Tenta executar o comando apenas se a coluna id não for identidade
+            EXECUTE format(
+                'DO $$ BEGIN
+                    IF NOT EXISTS (
+                        SELECT 1
+                        FROM information_schema.columns
+                        WHERE table_schema = ''public''
+                          AND table_name = ''%I''
+                          AND column_name = ''id''
+                          AND is_identity = ''YES''
+                    ) THEN
+                        ALTER TABLE %I ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1);
+                    END IF;
+                END $$;',
+                tbl.table_name, tbl.table_name
+            );
+        EXCEPTION
+            WHEN others THEN
+                RAISE NOTICE 'Skipped table: %', tbl.table_name;
+        END;
     END LOOP;
 END $$;
