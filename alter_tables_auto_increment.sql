173dabe663a7a923e304b1fe86169735cf88a5f3
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-01-21 23:16:51 +0000

    Update alter_tables_auto_increment.sql

--- alter_tables_auto_increment.sql
+++ alter_tables_auto_increment.sql
@@ -9,20 +9,18 @@ BEGIN
           AND table_schema = 'public'
     LOOP
         BEGIN
-            -- Verifica se a coluna id já é uma coluna de identidade antes de tentar alterar
-            IF NOT EXISTS (
-                SELECT 1
-                FROM information_schema.columns
-                WHERE table_schema = 'public'
-                  AND table_name = tbl.table_name
-                  AND column_name = 'id'
-                  AND is_identity = 'YES'
-            ) THEN
-                EXECUTE format(
-                    'ALTER TABLE %I ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1);',
-                    tbl.table_name
-                );
-            END IF;
+            -- Remove o valor padrão da coluna 'id'
+            EXECUTE format(
+                'ALTER TABLE %I ALTER COLUMN id DROP DEFAULT;',
+                tbl.table_name
+            );
+
+            -- Adiciona a propriedade de identidade à coluna 'id'
+            EXECUTE format(
+                'ALTER TABLE %I ALTER COLUMN id SET GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1);',
+                tbl.table_name
+            );
+
         EXCEPTION
             WHEN others THEN
                 RAISE NOTICE 'Erro ao processar tabela: %', tbl.table_name;
