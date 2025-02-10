d3d9378205f72330cf49af04e1c2b7d32922a7fc
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-02-10 21:41:57 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { scrapeResults3 } = require('./lesoes');
-const pool = require("./db");
 
 const app = express();
 // Configuração da porta para Render (usa a variável PORT ou padrão 3000)
