746d89149822701d7fea3c8bc9e08f57d83fa58c
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-02-19 11:23:21 +0000

    Update futebol.js

--- futebol.js
+++ futebol.js
@@ -588,8 +588,6 @@ for (let id of ids) {
             console.error("Erro geral no scraping:", error);
         }
     }
-
-    await browser.close();
 };
 // Exportando a função
 module.exports = {
